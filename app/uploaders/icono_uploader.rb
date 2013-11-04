1176585a5a2f5ae3138988a90b4ab1bbcda2879c
Ricardo <marcorcp1@hotmail.com>
2013-11-03 21:18:09 -0500

    Agregados estilos basados en los psd, versiones responsivas sin trabajar todavía

--- app/uploaders/icono_uploader.rb
+++ app/uploaders/icono_uploader.rb
@@ -36,6 +36,10 @@ class IconoUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [70, nil]
   end
 
+  version :thumb_servicios do
+    process :resize_to_limit => [70, 70] 
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
