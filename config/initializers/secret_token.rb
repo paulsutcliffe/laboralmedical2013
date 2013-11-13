7f38ea75165823c90515ab9b83350c034a3bcd3b
adriel <apinzasm@gmail.com>
2013-11-13 18:00:17 -0500

    Eliminado el secret_token de gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laboralmedical2013::Application.config.secret_token = '9b2f5aa662569ba182342f087bdcfcd36d0c1b937e602e923cef60ecf1de8ad2bd8641b78857052df6d1e284a9f935ae5c6826ab1915eacaa897e570a50a6f26'
